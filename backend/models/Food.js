cb18476ea01ff02063cde521821ccb3bd21bf865
tarkan3545 <tarkan.simsek0009@stud.hkr.se>
2026-04-25 19:52:05 +0200

    feat: create Food model

---
+++ backend/models/Food.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  calories: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Food", foodSchema);
